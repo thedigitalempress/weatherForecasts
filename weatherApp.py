7de371be5eedd6ccbc5b87c791f66d4974a30d66
thedigitalempress <33044102+thedigitalempress@users.noreply.github.com>
2018-03-09 16:10:35 -0500

    Update weatherApp.py

--- weatherApp.py
+++ weatherApp.py
@@ -1,6 +1,6 @@
 import urllib.request,json
 
-url = "https://api.openweathermap.org/data/2.5/weather?appid=2cfcab02ad439f3aa0dcfc20fd116ce7&q=Baton Rouge"
+url = "https://api.openweathermap.org/data/2.5/weather?appid=enter api key from owa here=city goes here"
 
 req= urllib.request.Request(url)
 print(urllib.request.urlopen(req).read())
